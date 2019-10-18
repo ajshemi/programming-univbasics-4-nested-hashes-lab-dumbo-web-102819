10682505faba31c625c92bf5073ba209575a8544
essential-hacker-4489 <akpojotor@yahoo.com>
2019-10-18 15:23:07 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -65,8 +65,7 @@ end
 
 def adding_matz
 # add the following information to the top level of programmer_hash
-  top_level {:yukihiro_matsumoto => {:known_for => "Ruby", :languages => ["LISP", "C"]
-  }
+  top_level ={:yukihiro_matsumoto => {:known_for => "Ruby", :languages => ["LISP", "C"] }
   }
 
 	programmer_hash = 
