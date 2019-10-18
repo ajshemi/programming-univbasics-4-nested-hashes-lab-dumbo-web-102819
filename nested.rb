6e21e768486f0025e836cfa4c3392f57b483c30f
essential-hacker-4489 <akpojotor@yahoo.com>
2019-10-18 15:09:02 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -19,7 +19,6 @@ def hopper
   
 
   programmer_hash[:grace_hopper]
-  programmer_hash[:alan_kay][:known_for]
 end
 
 def alan_kay_is_known_for
@@ -40,7 +39,8 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
-  programmer_hash[:grace_hopper].keys
+  programmer_hash[:alan_kay][:known_for]
+
 end
 
 def dennis_ritchies_language
